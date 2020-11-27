7f482ed50e4cdb5923f854aadb35b12e99e760b6
Rajeshroulo <rajraval017@gmail.com>
2020-11-27 10:38:19 +0530

    Added code to check the total records of the csv files

--- CensusAnalyser.php
+++ CensusAnalyser.php
@@ -1,46 +1,49 @@
 <?php
-  class CensusAnalyser
+class CensusAnalyser
+{
+  public function __construct()
   {
-	public function __construct()
-	{
-		echo "\n Welcome to Census Analyser Program \n";
-    } 
-    
-    function loadStateCensusData()
-    {
-      $arr=array();
-        if (($h = fopen("StateCensusData.csv", "r")) !== FALSE) {
-			      while (($data = fgetcsv($h, 1000, ",")) !== FALSE) {
+    echo "\n Welcome to Census Analyser Program \n";
+  }  
 
-             array_push($arr,$data);
-            }
-
-            print_r($arr);
-            
-			    // Close the file
-			    fclose($h);
-		    }
+  function loadStateCensusData($csvPath)
+  {
+    $row=0;
+    //open csv file in read mode
+    if (($h = fopen($csvPath, "r")) !== FALSE) {
+      // Convert each line into the local $data variable
+      while (($data = fgetcsv($h, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        $row++;                
+        for ($c = 0; $c < $num; $c++) {
+            $data[$c] . "\n";        //printing the contents of csv file
+        }
+      }
+      return ($row-1);
+      // Close the file
+      fclose($h);
     }
-              
-    function loadCensusData()
-    {
-      $array=array();
-      if (($h = fopen("StateCode.csv", "r")) !== FALSE) {
-			// Convert each line into the local $data variable
-			// The items of the array are comma separated
-            while (($data = fgetcsv($h, 1000, ",")) !== FALSE) {
-
-              array_push($array,$data);
-            } 
-            print_r($array); 
-			      // Close the file
-		    	fclose($h);
-		  }
-    } 
-
   }
 
-  $state_data = new CensusAnalyser();
-  $state_data->loadStateCensusData();
-  $state_data->loadCensusData();
+  function loadCensusData($statePath)
+  {
+    $row=0;
+    //open csv file in read mode
+    if (($h = fopen($statePath,"r")) !== FALSE) {
+      // Convert each line into the local $data variable
+      // The items of the array are comma separated
+      while (($data = fgetcsv($h, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        $row++;
+        for ($c = 0; $c < $num; $c++) {
+           $data[$c]. "\n" ;          //printing the contents of csv file
+        }
+      }
+      return ($row-1);
+      // Close the file
+      fclose($h);
+    }
+  }
+}
+
 ?>
--- CensusAnalyserTest.php
+++ CensusAnalyserTest.php
@@ -1,16 +1,29 @@
 <?php
-use PHPUnit\Framework\TestCase;
- class CensusAnalyserTest extends CensusAnalyser
- {
-     function total_records()
-     {
-        $analyser= new CensusAnalyser();
-        $records = $analyser->loadStateCensusData();
-        $this->assertSame(29,$records);
 
-     }
+require_once('CensusAnalyser.php');
 
- }
+class CensusAnalyserTest extends PHPUnit\Framework\TestCase
+{
+   static $csvPath = "StateCensusData.csv";
+   static $statePath = "StateCode.csv";
 
+   public function testTotalCsvRecords()
+   {
+      try {
+         $analyser = new CensusAnalyser();
+         $this->assertEquals(29, $analyser->loadStateCensusData(self::$csvPath));
+      } catch (Exception $e) {
+         echo "Csv Records not found";
+      }
+   }
 
-?>
+   public function testStateRecords()
+   {
+      try {
+         $analyser = new CensusAnalyser();
+         $this->assertEquals(37, $analyser->loadCensusData(self::$statePath));
+      } catch (Exception $e) {
+         echo "State Records not found";
+      }
+   }
+}
